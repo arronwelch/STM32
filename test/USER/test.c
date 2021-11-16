ca79c9f1dc77826965dd16054d2b86ad4aab4636
arronwelch <Davego@protonmail.com>
2021-11-17 01:40:55 +0800

    slow

--- test/USER/test.c
+++ test/USER/test.c
@@ -4,9 +4,9 @@
 int main(void)
 {
 	u8 t = 0;
-	Stm32_Clock_Init(9);
-	delay_init(72);
-	uart_init(72,9600);
+	Stm32_Clock_Init(9);//72M
+	delay_init(72);//delay init
+	uart_init(72,9600);//setting uart1 baud rate
 	while(1)
 	{
 		printf("t:%d\r\n",t);
