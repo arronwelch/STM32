aec10ecffbbbf03325a1490c8c540b89b603aabc
arronwelch <Davego@protonmail.com>
2021-11-21 00:40:55 +0800

    keil kill

--- test/OBJ/delay.d
+++
@@ -1,9 +0,0 @@
-..\obj\delay.o: ..\SYSTEM\delay\delay.c
-..\obj\delay.o: ..\SYSTEM\delay\delay.h
-..\obj\delay.o: ..\SYSTEM\sys\sys.h
-..\obj\delay.o: ..\SYSTEM\sys\stm32f10x.h
-..\obj\delay.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cm3.h
-..\obj\delay.o: E:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
-..\obj\delay.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmInstr.h
-..\obj\delay.o: E:\Keil_v5\ARM\PACK\ARM\CMSIS\3.20.4\CMSIS\Include\core_cmFunc.h
-..\obj\delay.o: ..\SYSTEM\sys\system_stm32f10x.h
