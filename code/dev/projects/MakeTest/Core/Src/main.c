9d41edce9edce78f6a43b5a84e5fb2408f26333e
arronwelch <Davego@protonmail.com>
2021-12-02 00:50:18 +0800

    use gccmake openocd program flash

--- code/dev/projects/MakeTest/Core/Src/main.c
+++ code/dev/projects/MakeTest/Core/Src/main.c
@@ -96,7 +96,11 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-    HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_8);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOD,GPIO_PIN_2,GPIO_PIN_RESET);
+    HAL_Delay(500);
+    HAL_GPIO_WritePin(GPIOA,GPIO_PIN_8,GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD,GPIO_PIN_2,GPIO_PIN_SET);
     HAL_Delay(500);
     /* USER CODE BEGIN 3 */
   }
